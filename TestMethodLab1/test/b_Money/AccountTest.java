53343e25b1680ad1e356e1fdddfe9b36bd7a5d44
parse <anders.hassis@gmail.com>
2010-11-22 15:30:52 +0100

    Kommentarer tillagda för test

--- TestMethodLab1/test/b_Money/AccountTest.java
+++ TestMethodLab1/test/b_Money/AccountTest.java
@@ -24,39 +24,47 @@ public class AccountTest {
 	
 	@Test
 	public void testAddRemoveTimedPayment() {
+		// Test addTimedPayment and check if it exists after function call
 		testAccount.addTimedPayment("Test", 1, 1, new Money(100, SEK), SweBank, "Alice");
 		assertTrue( testAccount.timedPaymentExists("Test") );
 		
+		// Try to remove timed payment and check if it is removed
 		testAccount.removeTimedPayment("Test");
 		assertFalse( testAccount.timedPaymentExists("Test") );
 	}
 	
 	@Test
 	public void testTimedPayment() throws AccountDoesNotExistException {
+		// The amounts before any ticks and payments
 		int preTransferAmountHans = testAccount.getBalance().getAmount().intValue();
 		int preTransferAmountAlice = SweBank.getBalance("Alice").intValue();
 		
+		// Set up the payment and tick once
 		testAccount.addTimedPayment("Test", 1, 1, new Money(100, SEK), SweBank, "Alice");
 		testAccount.tick();
 		
-		// Same amount
+		// There should be the same amount on each account
 		assertEquals(preTransferAmountHans, testAccount.getBalance().getAmount().intValue() );
 		assertEquals(preTransferAmountAlice, SweBank.getBalance("Alice").intValue() );
 		
 		testAccount.tick();
 		
-		// 100 withdrawn
+		// 100 withdrawn from testAccount. Alice gets deposit of 100
 		assertEquals(preTransferAmountHans-100, testAccount.getBalance().getAmount().intValue() );
 		assertEquals(preTransferAmountAlice+100, SweBank.getBalance("Alice").intValue() );
 		
+		// Tick for another payment
 		testAccount.tick();
 		testAccount.tick();
 		
+		// Another 100 withdrawn/deposit
 		assertEquals(preTransferAmountHans-200, testAccount.getBalance().getAmount().intValue() );
 		assertEquals(preTransferAmountAlice+200, SweBank.getBalance("Alice").intValue() );
 		
+		// Remove payment
 		testAccount.removeTimedPayment("Test");
 		
+		// Tick some times, and afterwards check if the amounts are still the same
 		testAccount.tick();
 		testAccount.tick();
 		testAccount.tick();
@@ -68,17 +76,21 @@ public class AccountTest {
 
 	@Test
 	public void testAddWithdraw() {
+		// Amount before deposit and withdraw
 		int preTransferAmountTestaccount = testAccount.getBalance().getAmount().intValue();
 		
+		// Try to deposit 200 and check if desired value
 		testAccount.deposit(new Money(200, SEK));
 		assertEquals(preTransferAmountTestaccount+200, testAccount.getBalance().getAmount().intValue() );
 		
+		// Try to withdraw 200 and check value
 		testAccount.withdraw(new Money(200, SEK));
 		assertEquals(preTransferAmountTestaccount, testAccount.getBalance().getAmount().intValue() );
 	}
 	
 	@Test
 	public void testGetBalance() {
+		// Test if we get the correct balance from an account
 		assertEquals(10000000, testAccount.getBalance().getAmount().intValue() );
 	}
 }
--- TestMethodLab1/test/b_Money/BankTest.java
+++ TestMethodLab1/test/b_Money/BankTest.java
@@ -24,68 +24,85 @@ public class BankTest {
 
 	@Test
 	public void testGetName() {
+		// Check that we get correct name
 		assertEquals("Nordea", Nordea.getName() );
 	}
 
 	@Test
 	public void testGetCurrency() {
+		// Try a true and a false statement of currency objects
 		assertSame(SEK, SweBank.getCurrency() );
 		assertNotSame(SEK, DanskeBank.getCurrency() );
 	}
 
 	@Test
 	public void testOpenAccount() throws AccountExistsException, AccountDoesNotExistException {		
+		// Try to open an account
 		SweBank.openAccount("Test");
+		// Try to deposit - If account is opened, we can do a deposit without getting exception
 		SweBank.deposit("Test", new Money(100, SEK) );
 	}
 
 	@Test
 	public void testDeposit() throws AccountDoesNotExistException {
+		// Try to deposit to an account of a bank and check that we get the desired balance
 		Nordea.deposit("Bob", new Money(100, SEK));
 		assertEquals(100, Nordea.getBalance("Bob").intValue() );
 		
+		// Try to deposit money of a currency other than the banks currency
 		Integer testDepositMultipleCurrencies = new Money(100, SEK).add(new Money(100, DKK)).getAmount();
-		
 		Nordea.deposit("Bob", new Money(100, DKK));
 		assertEquals(testDepositMultipleCurrencies.intValue(), Nordea.getBalance("Bob").intValue() );
 	}
 
 	@Test
 	public void testWithdraw() throws AccountDoesNotExistException {
+		// Set up a balance of +100
 		SweBank.deposit("Bob", new Money(100, SEK));
 		
+		// The expected value is 100 SEK - 100 DKK
 		Integer testDepositMultipleCurrencies = new Money(100, SEK).sub(new Money(100, DKK)).getAmount();
 		
+		// Try to withdraw DKK from SEK bank. Check if the value is the expected
 		SweBank.withdraw("Bob", new Money(100, DKK));
 		assertEquals(testDepositMultipleCurrencies.intValue(), SweBank.getBalance("Bob").intValue() );
 	}
 	
 	@Test
 	public void testGetBalance() throws AccountDoesNotExistException {
+		// Deposit 100 SEK to bob
 		Nordea.deposit("Bob", new Money(100, SEK));
+		
+		// We should have 100 as balance (SEK)
 		assertEquals(100, Nordea.getBalance("Bob").intValue() );
 	}
 	
 	@Test
-	public void testTransfer() throws AccountDoesNotExistException {		
+	public void testTransfer() throws AccountDoesNotExistException {	
+		// Get the current balances of two accounts
 		int preTransferAmountBob = SweBank.getBalance("Bob");
 		int preTransferAmountUlrika = SweBank.getBalance("Ulrika");
 		
+		// Transfer some money
 		SweBank.transfer("Bob", "Ulrika", new Money(100, SEK));
 		
+		// Check that money has been withdrawn
 		assertEquals(preTransferAmountBob-100, SweBank.getBalance("Bob").intValue());
+		// Check that deposit 
 		assertEquals(preTransferAmountUlrika+100, SweBank.getBalance("Ulrika").intValue());
 	}
 	
 	@Test
 	public void testTimedPayment() throws AccountDoesNotExistException {
+		// Get the current balances of two accounts
 		int preTransferAmountBob = SweBank.getBalance("Bob");
 		int preTransferAmountBobNordea = Nordea.getBalance("Bob");
 		
+		// Add a timed payment and tick
 		SweBank.addTimedPayment("Bob", "Test", 1, 1, new Money(100, SEK), Nordea, "Bob");
 		SweBank.tick();
 		
-		// Same amount
+		// Should be same amounts
 		assertEquals(preTransferAmountBob, SweBank.getBalance("Bob").intValue());
 		assertEquals(preTransferAmountBobNordea, Nordea.getBalance("Bob").intValue());
 		
@@ -93,14 +110,18 @@ public class BankTest {
 		
 		// 100 withdrawn
 		assertEquals(preTransferAmountBob-100, SweBank.getBalance("Bob").intValue());
+		// 100 deposit
 		assertEquals(preTransferAmountBobNordea+100, Nordea.getBalance("Bob").intValue());
 		
 		SweBank.tick();
 		SweBank.tick();
 		
+		// 200 withdrawn
 		assertEquals(preTransferAmountBob-200, SweBank.getBalance("Bob").intValue());
+		// 200 deposit
 		assertEquals(preTransferAmountBobNordea+200, Nordea.getBalance("Bob").intValue());
 		
+		// Remove the payment
 		SweBank.removeTimedPayment("Bob", "Test");
 		
 		SweBank.tick();
@@ -108,6 +129,7 @@ public class BankTest {
 		SweBank.tick();
 		SweBank.tick();
 		
+		// Check that the amounts are the same as last time, so we know the payment has been removed
 		assertEquals(preTransferAmountBob-200, SweBank.getBalance("Bob").intValue());
 		assertEquals(preTransferAmountBobNordea+200, Nordea.getBalance("Bob").intValue());
 	}
