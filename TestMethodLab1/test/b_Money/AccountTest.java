05bb4aafed7092ded06c2f92d2ffaf6a331d1f2f
parse <anders.hassis@gmail.com>
2010-11-22 12:48:07 +0100

    Accounttest har en färdig svit

--- TestMethodLab1/test/b_Money/AccountTest.java
+++ TestMethodLab1/test/b_Money/AccountTest.java
@@ -24,21 +24,61 @@ public class AccountTest {
 	
 	@Test
 	public void testAddRemoveTimedPayment() {
-		fail("Write test case here");
+		testAccount.addTimedPayment("Test", 1, 1, new Money(100, SEK), SweBank, "Alice");
+		assertTrue( testAccount.timedPaymentExists("Test") );
+		
+		testAccount.removeTimedPayment("Test");
+		assertFalse( testAccount.timedPaymentExists("Test") );
 	}
 	
 	@Test
 	public void testTimedPayment() throws AccountDoesNotExistException {
-		fail("Write test case here");
+		int preTransferAmountHans = testAccount.getBalance().getAmount().intValue();
+		int preTransferAmountAlice = SweBank.getBalance("Alice").intValue();
+		
+		testAccount.addTimedPayment("Test", 1, 1, new Money(100, SEK), SweBank, "Alice");
+		testAccount.tick();
+		
+		// Same amount
+		assertEquals(preTransferAmountHans, testAccount.getBalance().getAmount().intValue() );
+		assertEquals(preTransferAmountAlice, SweBank.getBalance("Alice").intValue() );
+		
+		testAccount.tick();
+		
+		// 100 withdrawn
+		assertEquals(preTransferAmountHans-100, testAccount.getBalance().getAmount().intValue() );
+		assertEquals(preTransferAmountAlice+100, SweBank.getBalance("Alice").intValue() );
+		
+		testAccount.tick();
+		testAccount.tick();
+		
+		assertEquals(preTransferAmountHans-200, testAccount.getBalance().getAmount().intValue() );
+		assertEquals(preTransferAmountAlice+200, SweBank.getBalance("Alice").intValue() );
+		
+		testAccount.removeTimedPayment("Test");
+		
+		testAccount.tick();
+		testAccount.tick();
+		testAccount.tick();
+		testAccount.tick();
+		
+		assertEquals(preTransferAmountHans-200, testAccount.getBalance().getAmount().intValue() );
+		assertEquals(preTransferAmountAlice+200, SweBank.getBalance("Alice").intValue() );
 	}
 
 	@Test
 	public void testAddWithdraw() {
-		fail("Write test case here");
+		int preTransferAmountTestaccount = testAccount.getBalance().getAmount().intValue();
+		
+		testAccount.deposit(new Money(200, SEK));
+		assertEquals(preTransferAmountTestaccount+200, testAccount.getBalance().getAmount().intValue() );
+		
+		testAccount.withdraw(new Money(200, SEK));
+		assertEquals(preTransferAmountTestaccount, testAccount.getBalance().getAmount().intValue() );
 	}
 	
 	@Test
 	public void testGetBalance() {
-		fail("Write test case here");
+		assertEquals(10000000, testAccount.getBalance().getAmount().intValue() );
 	}
 }
